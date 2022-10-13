c13a257e1f5534e646df0da66554e6addba86dda
Mike Massie <mike@michaelmassie.com>
2022-10-13 17:48:34 -0500

    push for CICD

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 exports.handler = async (event) => {
     // TODO implement
+    // Test for CICD solution in Github
     const response = {
         statusCode: 200,
         body: JSON.stringify('Hello Mike from the Trailpost Lambda API Server!'),
