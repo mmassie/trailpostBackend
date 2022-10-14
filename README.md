779558fa30b14c329099c5eb499867bfdd14a974
Mike Massie <mike@michaelmassie.com>
2022-10-14 07:36:14 -0500

    minor cleaning

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Trailpost Back-End
 Lambda API Backend to Trailpost Digital Assistant
-- [x] Github Actions to Lambda
-- [ ] Pass PV intent to Lambda to execute query
+
+This service is intented to connect the Trailforks app to Digital Assistants to get trail conditions.
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello Mike,from the Trailpost Lambda API Server via GitHub!'),
+        body: JSON.stringify('Hello Mike,from the Trailpost Lambda API Server, via GitHub!'),
     };
     return response;
 };
