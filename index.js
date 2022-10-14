96fdc6c5e22e4643fa5c94526e9e8dc548256b55
Mike Massie <mike@michaelmassie.com>
2022-10-13 21:10:12 -0500

    test message

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 exports.handler = async (event) => {
     // TODO implement
+    // Github Actions to Lambda Test
     const response = {
         statusCode: 200,
         body: JSON.stringify('Hello Mike from the Trailpost Lambda API Server!'),
