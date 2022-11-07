4cd561807af72d934939dc5498ed55655db4b516
Mike Massie <mike@michaelmassie.com>
2022-11-07 17:01:17 -0600

    payload response test 2

--- index.js
+++ index.js
@@ -4,18 +4,32 @@ exports.handler = async (event) => {
         statusCode: 200,
         // body: JSON.stringify('Hello Mike,from the Trailpost Lambda API Server, via GitHub!'),
         body: JSON.stringify(
-            app.handle('rich_response', conv => {
-                conv.add('This is a card rich response - but also, hello from Lambda.');
-                conv.add(new Card({
-                  title: 'Card Title',
-                  subtitle: 'Card Subtitle',
-                  text: 'Card Content',
-                  image: new Image({
-                    url: 'https://developers.google.com/assistant/assistant_96.png',
-                    alt: 'Google Assistant logo'
-                  })
-                }));
-              });
+            {
+                "session": {
+                  "id": "example_session_id",
+                  "params": {}
+                },
+                "prompt": {
+                  "override": false,
+                  "content": {
+                    "card": {
+                      "title": "Card Title",
+                      "subtitle": "Card Subtitle",
+                      "text": "Card Content",
+                      "image": {
+                        "alt": "Google Assistant logo",
+                        "height": 0,
+                        "url": "https://developers.google.com/assistant/assistant_96.png",
+                        "width": 0
+                      }
+                    }
+                  },
+                  "firstSimple": {
+                    "speech": "This is a card rich response.",
+                    "text": ""
+                  }
+                }
+              }
               
         ),
     };
