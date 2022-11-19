00ebe88f3ffd05311f5ec0bec9f9572b42bed4b1
Mike Massie <mike@michaelmassie.com>
2022-11-19 12:59:22 -0600

    make-a da-calls

--- index.js
+++ index.js
@@ -1,4 +1,29 @@
-exports.handler = async (event, intent) => {
+const https = require('https');
+
+let options = {
+    host : 'trailforks.com',
+    path:  '/widgets/trails_status/?rid=38974&data-displaytype=table&dataunsanctioned=1&data-officialonly=0&data-active=0&data-d=1&data-c=0&data-p=0',
+};
+
+exports.handler = (event, context, callback) => {
+    const req = https.request(options, (res) => {
+        let body = '';
+        console.log('Status:', res.statusCode);
+        console.log('Headers:', JSON.stringify(res.headers));
+        res.setEncoding('utf8');
+        res.on('data', (chunk) => body += chunk);
+        res.on('end', () => {
+            console.log('Successfully processed HTTPS response');
+            // If we know it's JSON, parse it
+            if (res.headers['content-type'] === 'application/json') {
+                body = JSON.parse(body);
+            }
+            callback(null, body);
+        });
+    });
+    req.on('error', callback);
+    req.write(JSON.stringify(event.data));
+    req.end();
     const response = {
         statusCode: 200,
         body: JSON.stringify(
@@ -27,18 +52,3 @@ exports.handler = async (event, intent) => {
     };
     return response;
 };
-
-
-var req = require('request');
-const params = {
-    url: 'https://www.trailforks.com/widgets/trails_status/?rid=38974&data-displaytype=table&dataunsanctioned=1&data-officialonly=0&data-active=0&data-d=1&data-c=0&data-p=0',
-    headers: { 'Content-Type': 'application/json' },
-    json: JSON.parse({ id: 1})
-};
-req.post(params, function(err, res, body) {
-    if(err){
-        console.log('------error------', err);
-    } else{
-        console.log('------success--------', body);
-    }
-});
