eaa576ea0147595e87e6bbff470abc16cac6e9ad
Mike Massie <mike@michaelmassie.com>
2022-11-24 16:56:41 -0600

    moar darts

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const flowerp = 523733;
 const landmine = 5009;
 const rusty = 5012;
 
-let condition = {
+let conditionObj() = {
     0 : "Unknown",
     10 : "Snow Groomed",
     1 : "Snow Packed",
@@ -70,7 +70,7 @@ exports.handler = async event => {
             "prompt": {
               "override": false,
               "firstSimple": {
-                "speech": "Got it. Trail conditions at " + result.data.title + " are rated at " + result.data.condition,
+                "speech": "Got it. Trail conditions at " + result.data.title + " are currently rated at " + conditionObj(result.data.condition),
                 "text": ""
               }
             },
