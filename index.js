916ec53da12ad1da263d4283cfd68a3e08524b26
Mike Massie <mike@michaelmassie.com>
2022-11-24 14:52:08 -0600

    finding the proper JSON return Object to pass thru

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const https = require('https');
 
+//Trail intents to trailid
+const pleasantv = 140133;
+const laphamp = 268524;
+const minooka = 50215;
+const hurl = 5007;
+const flowerp = 523733;
+const landmine = 5009;
+const rusty = 5012;
+
 function getRequest() {
   const url = 'https://www.trailforks.com/api/1/trail?id=140133&scope=full&api_key=docs';
 
@@ -26,7 +35,7 @@ function getRequest() {
   });
 }
 
-exports.handler = async event => {
+/* exports.handler = async event => {
   try {
     const result = await getRequest();
     console.log('result is: 👉️', result);
@@ -38,10 +47,41 @@ exports.handler = async event => {
       body: JSON.stringify(result),
     };
   } catch (error) {
-    console.log('Error is probably HTTP instead of JSON: 👉️', error);
+    console.log('Error is: 👉️', error);
     return {
       statusCode: 203,
       body: error.message,
     };
   }
+  */
+  exports.handler = async (event, intent) => {
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify(
+            {
+                "session": {
+                  "id": "001",
+                  "params": {}
+                },
+                "prompt": {
+                  "override": false,
+                  "firstSimple": {
+                    "speech": res.on.alias // "Got it. Trail conditions at Pleasant Valley are Dry and All Clear as of July 22",
+                    "text": ""
+                  }
+                },
+                "scene": {
+                  "name": "SceneName",
+                  "slots": {},
+                  "next": {
+                    "name": "actions.scene.END_CONVERSATION"
+                  }
+                }
+              }
+              
+        ),
+    };
+    return response;
+};
+
 };
