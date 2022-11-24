b431960a1aa35254d6ed04c505d097ddf8217930
Mike Massie <mike@michaelmassie.com>
2022-11-24 16:11:36 -0600

    throwing darts

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const flowerp = 523733;
 const landmine = 5009;
 const rusty = 5012;
 
-const conditionObj = {
+function conditionObj(result.data.condition) = {
     0 : "Unknown",
     10 : "Snow Groomed",
     1 : "Snow Packed",
@@ -70,7 +70,7 @@ exports.handler = async event => {
             "prompt": {
               "override": false,
               "firstSimple": {
-                "speech": "Got it. Trail conditions at " + result.data.title + " are rated at " + conditionObj(result.data.condition),
+                "speech": "Got it. Trail conditions at " + result.data.title + " are rated at " + conditionObj,
                 "text": ""
               }
             },
