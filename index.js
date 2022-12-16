e23c84a42530e6797ae8644fa3dd9af5bac7640f
Mike Massie <mike@michaelmassie.com>
2022-12-16 10:18:24 -0600

    result log

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ exports.handler = async event => {
     const result = await getRequest();
     console.log(JSON.stringify(event));
     console.log(JSON.stringify(JSON.parse(event.body)));
-    console.log(result.intent.name)
+    console.log(result)
 
     return {
       statusCode: 200,
