b3b1c8dc0d5a48ddff0f5843d25f96aab2c75717
Mike Massie <mike@michaelmassie.com>
2022-12-16 10:09:34 -0600

    event body inent name test

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ exports.handler = async event => {
     const result = await getRequest();
     console.log(JSON.stringify(event));
     console.log(JSON.stringify(JSON.parse(event.body)));
-    console.log (result)
+    console.log(event.body.intent.name);
 
     return {
       statusCode: 200,
